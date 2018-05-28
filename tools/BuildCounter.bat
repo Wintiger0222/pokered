3a2429ae846519e2d2d60a5b77cb9ed4d67f8a67
뇌씨 <009342@naver.com>
2018-05-29 01:54:56 +0900

    Apply Hangul Output Improvement Code
    From master branch

--- tools/BuildCounter.bat
+++ tools/BuildCounter.bat
@@ -4,6 +4,8 @@ set /p BuildNumber=<BuildNumber.txt
 set /a BuildNumber+=1
 echo %BuildNumber% > BuildNumber.txt
 echo db "%BuildNumber%@" > ..\BuildNumber.asm
-echo db "%date% %time%@" > ..\BuildTime.asm
+echo db "%date%@" > ..\BuildDate.asm
+echo db "%time%@" > ..\BuildTime.asm
 echo BuildNumber : %BuildNumber%
-echo BuildTime : %date% %time%
+echo BuildDate : %date%
+echo BuildTime : %time%
