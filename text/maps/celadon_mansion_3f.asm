b0897c4a9a0d6f67a3ceaddb2f5ceadb77140895
WindowsTiger <0xodbs02@naver.com>
2018-05-31 13:30:41 +0900

    Update celadon_mansion_3f.asm

--- text/maps/celadon_mansion_3f.asm
+++ text/maps/celadon_mansion_3f.asm
@@ -1,122 +1,116 @@
 _ProgrammerText::
-	text "Me? I'm the"
-	line "programmer!"
+	text $02,$3A,$04,$4B,$07,$9C,$02,$CF,$0B,$67; RAW DATA : 	text "나말이니?"
+	line $02,$3D,$7F,$0A,$61,$03,$FE,$01,$A7,$03,$D1,$04,$73,$03,$C5,$02,$D9,$0B,$66; RAW DATA : 	line "난 프로그래머란다!"
 	done
 
 IF DEF(_YELLOW)
 _ProgrammerText2::
-	text "Me? I'm the"
-	line "programmer!"
+	text $02,$3A,$04,$4B,$07,$9C,$02,$CF,$0B,$67; RAW DATA : 	text "나말이니?"
+	line $02,$3D,$7F,$0A,$61,$03,$FE,$01,$A7,$03,$D1,$04,$73,$03,$C5,$02,$D9,$0B,$66; RAW DATA : 	line "난 프로그래머란다!"
 
-	para "What a surprise!"
-	line "I never expected"
-	cont "anyone to fill a"
-	cont "#DEX."
-	done	
+	para $07,$9C,$01,$25,$7F,$02,$8E,$03,$C3,$07,$4E,$01,$29,$0B,$66; RAW DATA : 	para "이거 놀라운걸!"
+	line $06,$EE,$02,$C0,$7F,$02,$A9,$01,$88,$03,$15,$7F,$0A,$27,$09,$2F,$04,$93,$7F,$03,$15,$01,$08,$07,$8B; RAW DATA : 	line "어느 누구도 포켓몬 도감을"
+	cont $07,$2F,$05,$EA,$0A,$72,$01,$25,$03,$C3,$01,$4F,$7F,$07,$19,$05,$C3,$03,$15,$7F,$04,$98,$0A,$7F,$06,$EE; RAW DATA : 	cont "완성할거라곤 예상도 못했어"
+	done
 ENDC
 _GraphicArtistText::
-	text "I'm the graphic"
-	line "artist!"
-	cont "I drew you!"
+	text $02,$3A,$02,$C2,$7F,$01,$A7,$03,$D1,$0A,$68,$7F,$03,$60,$07,$AA,$07,$9C,$02,$6A,$0B,$66; RAW DATA : 	text "나는 그래픽 디자이너!"
+	line $02,$6A,$04,$36,$7F,$01,$A7,$04,$40,$7F,$01,$2D,$07,$8A,$7F,$02,$3A,$03,$C5,$02,$D9,$0B,$66; RAW DATA : 	line "너를 그린 것은 나란다!"
 	done
+	
 IF DEF(_YELLOW)
 _GraphicArtistText2::
-	text "I'm the graphic"
-	line "artist!"
+	text $02,$3A,$02,$C2,$7F,$01,$A7,$03,$D1,$0A,$68,$7F,$03,$60,$07,$AA,$07,$9C,$02,$6A,$0B,$66; RAW DATA : 	text "나는 그래픽 디자이너!"
 
-	para "Wow, you finished"
-	line "your #DEX!"
-	cont "Want me to PRINT"
-	cont "out a DIPLOMA"
-	cont "as proof?"
+	para $07,$20,$7F,$07,$2F,$05,$EA,$0A,$7F,$01,$88,$02,$3A,$0B,$66; RAW DATA : 	para "오 완성했구나!"
+	line $0A,$27,$09,$2F,$04,$93,$7F,$03,$15,$01,$08,$07,$8B,$7F,$07,$2F,$05,$EA,$0A,$71,$01,$B2,$02,$84,$07,$88,$03,$FE; RAW DATA : 	line "포켓몬 도감을 완성한기념으로"
+	cont $0A,$35,$08,$A2,$07,$B5,$07,$8B,$7F,$0A,$61,$04,$40,$09,$DE,$7F,$0A,$78,$08,$09,$01,$BE,$0B,$67; RAW DATA : 	cont "표창장을 프린트 해줄까?"
 	done
 
 _GraphicArtistText3::
-	text "Just tell me if"
-	line "you want to PRINT"
-	cont "out a DIPLOMA."
+	text $0A,$35,$08,$A2,$07,$B5,$07,$8B,$7F,$0A,$61,$04,$40,$09,$DE,$7F,$0A,$6F,$01,$4D,$7F,$06,$6D,$07,$88,$04,$89; RAW DATA : 	text "표창장을 프린트 하고 싶으면"
+	line $02,$4B,$01,$34,$7F,$04,$4B,$0A,$6F,$03,$F5; RAW DATA : 	line "내게 말하렴"
 	done
 
 _GraphicArtistText4::
-	text "All done!"
+	text $02,$D9,$03,$26,$02,$D9,$0B,$66,$7F,$01,$01,$08,$26,$03,$F5,$0B,$66; RAW DATA : 	text "다됐다! 가지렴!"
 	done
 
 _GraphicArtistText5::
-	text "OK, let's not"
-	line "PRINT."
+	text $01,$A7,$03,$D1; RAW DATA : 	text "그래"
+	line $0A,$61,$04,$40,$09,$DE,$0A,$6F,$08,$26,$7F,$06,$CA,$07,$88,$04,$46; RAW DATA : 	line "프린트하지 않으마"
 	done
 
 ENDC
 _WriterText::
-	text "I wrote the story!"
-	line "Isn't ERIKA cute?"
+	text $02,$3A,$02,$C2,$7F,$06,$4A,$09,$B4,$04,$3E,$04,$36,$7F,$06,$B4,$02,$DC,$02,$D9,$0B,$66; RAW DATA : 	text "나는 스토리를 쓴단다!"
+	line $04,$CE,$0A,$AD,$7F,$01,$9D,$07,$11,$08,$26,$7F,$06,$CA,$06,$C6,$0B,$67; RAW DATA : 	line "민화 귀엽지 않아?"
 
-	para "I like MISTY a"
-	line "lot too!"
-
-	para "Oh, and SABRINA,"
-	line "I like her!"
+	para $07,$9C,$06,$4D,$07,$9C,$03,$15,$7F,$07,$D4,$04,$4B,$7F,$07,$F1,$06,$C6,$0B,$66; RAW DATA : 	para "이슬이도 정말 좋아!"
+	line $06,$C6,$0B,$6B,$01,$A7,$04,$3E,$01,$4D,$7F,$08,$CA,$03,$F3,$03,$15,$7F,$07,$F1,$06,$C6,$0B,$66; RAW DATA : 	line "아,그리고 초련도 좋아!"
 	done
 
 IF DEF(_YELLOW)
 _WriterText2::
-	text "I wrote the story!"
+	text $02,$3A,$02,$C2,$7F,$06,$4A,$09,$B4,$04,$3E,$04,$36,$7F,$06,$B4,$02,$DC,$02,$D9,$0B,$66; RAW DATA : 	text "나는 스토리를 쓴단다!"
 
-	para "It's great you"
-	line "caught all the"
-	cont "#MON! Thanks!"
+	para $07,$D4,$04,$4B,$7F,$01,$82,$07,$B5,$0A,$6F,$01,$4D,$7F,$01,$4D,$04,$4F,$01,$88,$02,$3A,$0B,$66; RAW DATA : 	para "정말 굉장하고 고맙구나!"
+	line $02,$77,$01,$01,$7F,$04,$90,$03,$47,$7F,$0A,$27,$09,$2F,$04,$93,$07,$8B,$7F,$07,$B2,$06,$D2,$06,$EE,$0B,$66; RAW DATA : 	line "네가 모든 포켓몬을 잡았어!"
 	done
 
 ENDC
 _GameDesignerText::
-	text "Is that right?"
+	text $01,$A7,$08,$6A,$07,$8A,$7F,$07,$AF,$7F,$03,$27,$01,$4D,$7F,$07,$A6,$06,$EE,$0B,$67; RAW DATA : 	text "그쪽은 잘 되고 있어?"
 
-	para "I'm the game"
-	line "designer!"
+	para $02,$3A,$02,$C2,$7F,$01,$34,$07,$A3,$03,$60,$07,$AA,$07,$9C,$02,$6A,$03,$C5,$02,$D9,$0B,$66; RAW DATA : 	para "나는 게임디자이너란다!"
 
-	para "Filling up your"
-	line "#DEX is tough,"
-	cont "but don't quit!"
+	para $0A,$27,$09,$2F,$04,$93,$7F,$03,$15,$01,$08,$07,$8B,$7F,$07,$2F,$05,$EA,$0A,$6F,$02,$C2,$7F,$01,$2D,$07,$8A; RAW DATA : 	para "포켓몬 도감을 완성하는 것은"
+	line $0A,$FB,$03,$49,$01,$2D,$07,$9C,$03,$C5,$02,$D9; RAW DATA : 	line "힘들것이란다"
+	cont $0A,$6F,$08,$26,$04,$48,$7F,$0A,$27,$01,$B2,$0A,$6F,$08,$26,$7F,$04,$46,$03,$F5,$0B,$66; RAW DATA : 	cont "하지만 포기하지 마렴!"
 
-	para "When you finish,"
-	line "come tell me!"
+	para $03,$15,$01,$08,$7F,$07,$2F,$05,$EA,$07,$8B,$7F,$02,$31,$02,$4B,$04,$89; RAW DATA : 	para "도감 완성을 끝내면"
+	line $07,$2D,$05,$DD,$7F,$02,$4B,$01,$34,$7F,$04,$4B,$0A,$78,$08,$06,$03,$F5,$0B,$66; RAW DATA : 	line "와서 내게 말해주렴!"
 	done
 
 _CompletedDexText::
-	text "Wow! Excellent!"
-	line "You completed"
-	cont "your #DEX!"
-	cont "Congratulations!"
-	cont "...@@"
+	text $01,$82,$07,$B5,$0A,$78,$0B,$66; RAW DATA : 	text "굉장해!"
+	line $0A,$27,$09,$2F,$04,$93,$7F,$03,$15,$01,$08,$7F,$07,$2F,$05,$EA,$07,$8B; RAW DATA : 	line "포켓몬 도감 완성을"
+	cont $05,$EA,$01,$68,$0A,$7F,$02,$D9,$02,$C2,$01,$25,$08,$26,$0B,$67; RAW DATA : 	cont "성공했다는거지?"
+	
+	para $08,$E0,$0A,$6F,$0A,$71,$02,$D9,$0B,$66,$50,$50; RAW DATA : 	para "축하한다!@@"
+
 IF DEF(_YELLOW)
 _CompletedDexText2::
-	text "Go show off your"
-	line "DIPLOMA to"
-	cont "the development"
-	cont "crew."
+	text $01,$13,$04,$DF,$08,$28,$03,$49,$07,$01,$01,$34,$7F,$02,$77,$7F,$0A,$35,$08,$A2,$07,$B5,$07,$8B; RAW DATA : 	text "개발진들에게 네 표창장을"
+	line $05,$E1,$05,$18,$07,$9C,$02,$C2,$01,$34,$7F,$07,$F1,$01,$3A,$06,$EE; RAW DATA : 	line "선보이는게 좋겠어"
 	done
 ENDC
 
 _CeladonMansion3Text5::
-	text "It's the game"
-	line "program! Messing"
-	cont "with it could bug"
-	cont "out the game!"
+	text $07,$9C,$01,$27,$7F,$01,$34,$07,$A3,$7F,$0A,$61,$03,$FE,$01,$A7,$03,$D5,$07,$9C,$06,$DF,$0B,$66; RAW DATA : 	text "이건 게임 프로그램이야!"
+	line $07,$9C,$7F,$01,$34,$07,$A3,$07,$01,$02,$C2,$7F,$04,$F6,$01,$A7,$01,$01; RAW DATA : 	line "이 게임에는 버그가"
+	cont $06,$F4,$0A,$94,$07,$A6,$07,$8B,$06,$26,$03,$15,$7F,$07,$A6,$06,$EE,$0B,$66; RAW DATA : 	cont "얽혀있을수도 있어!"
+	
+	para $0A,$27,$09,$2F,$04,$93,$06,$4A,$09,$9D,$7F,$0A,$67,$09,$0B,$08,$F2,$7F,$04,$F6,$07,$CC; RAW DATA : 	para "포켓몬스터 피카츄 버전"
+	line $0A,$71,$01,$AB,$0A,$AD,$0B,$62,$96,$A8,$AD,$A3,$AE,$B6,$B2,$93,$A8,$A6,$A4,$B1; RAW DATA : 	line "한글화『WindowsTiger"
+	cont $02,$9A,$06,$BE,$7F,$8C,$93,$86,$7F,$07,$8A,$05,$B7,$08,$B6; RAW DATA : 	cont "뇌씨 MTG 은사철"
+	
+	para $0A,$61,$03,$FE,$01,$A7,$03,$D1,$04,$D6,$7F,$0A,$6F,$02,$C0,$03,$C3,$7F,$01,$4D,$05,$CD,$0A,$6F,$06,$65; RAW DATA : 	para "프로그래밍 하느라 고생하신"
+	line $02,$9A,$06,$BE,$02,$D4,$7F,$01,$08,$05,$B7,$0A,$75,$02,$CF,$02,$D9; RAW DATA : 	line "뇌씨님 감사합니다"
 	done
 
+
 _CeladonMansion3Text6::
-	text "Someone's playing"
-	line "a game instead of"
-	cont "working!"
+	text $02,$A9,$01,$8A,$01,$01,$07,$01,$01,$34,$02,$C2; RAW DATA : 	text "누군가에게는"
+	line $02,$8E,$01,$25,$04,$3E,$07,$9E,$7F,$01,$34,$07,$A3,$07,$9C,$08,$26,$04,$48; RAW DATA : 	line "놀거리인 게임이지만"
+	cont $02,$4B,$01,$35,$7F,$08,$27,$06,$F7,$07,$9C,$08,$26,$0B,$66; RAW DATA : 	cont "내겐 직업이지!"
 	done
 
 _CeladonMansion3Text7::
-	text "It's the script!"
-	line "Better not look"
-	cont "at the ending!"
+	text $07,$9C,$01,$27,$7F,$06,$4A,$09,$79,$04,$43,$09,$DE,$06,$DF,$0B,$66; RAW DATA : 	text "이건 스크립트야!"
+	line $07,$03,$03,$69,$07,$8B,$7F,$06,$C8,$05,$18,$02,$C2,$7F,$01,$34,$7F,$02,$44,$01,$3A,$06,$EE,$0B,$66; RAW DATA : 	line "엔딩을 안보는 게 낫겠어!"
 	done
 
 _CeladonMansion3Text8::
-	text "GAME FREAK"
-	line "Development Room"
+	text $01,$34,$07,$A3,$7F,$0A,$61,$04,$3E,$09,$79,$7F,$01,$13,$04,$DF,$06,$67; RAW DATA : 	text "게임 프리크 개발실"
 	done
