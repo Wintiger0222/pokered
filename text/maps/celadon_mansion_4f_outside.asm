4cc999b30eaea8d608fa9cb0931aaeb17eb5539c
WindowsTiger <0xodbs02@naver.com>
2018-05-31 13:31:32 +0900

    Update celadon_mansion_4f_outside.asm

--- text/maps/celadon_mansion_4f_outside.asm
+++ text/maps/celadon_mansion_4f_outside.asm
@@ -1,3 +1,3 @@
 _CeladonMansion4Text1::
-	text "I KNOW EVERYTHING!"
+	text $02,$3A,$02,$C2,$7F,$04,$90,$03,$47,$01,$2D,$07,$8B,$7F,$06,$CB,$01,$4D,$7F,$07,$A6,$02,$D9,$0B,$66; RAW DATA : 	text "나는 모든것을 알고 있다!"
 	done
