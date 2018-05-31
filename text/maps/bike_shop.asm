4fc65d1fcacb48ccd55eed201e5aaa29b0c22bd8
WindowsTiger <0xodbs02@naver.com>
2018-05-31 13:14:46 +0900

    Update bike_shop.asm

--- text/maps/bike_shop.asm
+++ text/maps/bike_shop.asm
@@ -1,78 +1,67 @@
 _BikeShopText_1d810::
-	text "Hi! Welcome to"
-	line "our BIKE SHOP."
+	text $06,$EE,$05,$DD,$07,$20,$03,$F5,$0B,$66,$7F,$04,$CC,$03,$C3,$09,$7C,$7F,$05,$B7,$07,$9C,$09,$7C,$07,$01; RAW DATA : 	text "어서오렴! 미라클 사이클에"
+	line $07,$22,$01,$29,$7F,$0A,$AF,$07,$15,$0A,$71,$02,$DC,$02,$D9,$0B,$66; RAW DATA : 	line "온걸 환영한단다!"
 
-	para "Have we got just"
-	line "the BIKE for you!"
+	para $07,$09,$01,$B2,$07,$01,$02,$C2,$7F,$07,$20,$08,$27,$7F,$02,$6A,$04,$48,$07,$8B,$7F,$07,$77,$0A,$71; RAW DATA : 	para "여기에는 오직 너만을 위한"
+	line $07,$AA,$07,$CC,$01,$25,$01,$01,$7F,$07,$A6,$02,$DC,$02,$D9,$0B,$66; RAW DATA : 	line "자전거가 있단다!"
 	prompt
 
 _BikeShopText_1d815::
-	text "It's a cool BIKE!"
-	line "Do you want it?"
+	text $07,$09,$01,$B2,$7F,$04,$7A,$08,$28,$7F,$07,$AA,$07,$CC,$01,$25,$01,$01,$7F,$07,$A6,$02,$C2,$03,$05; RAW DATA : 	text "여기 멋진 자전거가 있는데"
+	line $01,$0E,$01,$4D,$7F,$06,$6D,$08,$26,$7F,$06,$CA,$02,$CF,$0B,$67; RAW DATA : 	line "갖고 싶지 않니?"
 	done
 
 _BikeShopCantAffordText::
-	text "Sorry! You can't"
-	line "afford it!"
+	text $04,$CC,$06,$C8,$0A,$6F,$01,$88,$02,$3A; RAW DATA : 	text "미안하구나"
+	line $03,$17,$07,$9C,$7F,$04,$90,$07,$AA,$04,$33,$01,$88,$02,$3A; RAW DATA : 	line "돈이 모자르구나"
 	prompt
 
 _BikeShopText_1d81f::
-	text "Oh, that's..."
+	text $06,$EE,$7F,$01,$A7,$01,$27,$0B,$6A,$0B,$6A; RAW DATA : 	text "어 그건……"
 
-	para "A BIKE VOUCHER!"
+	para $07,$AA,$07,$CC,$01,$25,$7F,$01,$83,$0A,$AF,$01,$97,$0B,$66; RAW DATA : 	para "자전거 교환권!"
 
-	para "OK! Here you go!"
+	para $07,$F1,$06,$C6,$0B,$66,$7F,$07,$09,$01,$B2,$07,$A6,$02,$DC,$02,$D9; RAW DATA : 	para "좋아! 여기있단다"
 	prompt
 
 _BikeShopText_1d824::
-	text "<PLAYER> exchanged"
-	line "the BIKE VOUCHER"
-	cont "for a BICYCLE.@@"
+	text $52, $02,$C2,$0B,$64,$07,$8A,$0B,$65; RAW DATA : 	text $52, "는(은)"
+	line $01,$83,$0A,$AF,$01,$97,$07,$88,$03,$FE; RAW DATA : 	line "교환권으로"
+	cont $07,$AA,$07,$CC,$01,$25,$04,$36,$7F,$06,$F2,$06,$FA,$02,$D9,$0B,$66,$50,$50; RAW DATA : 	cont "자전거를 얻었다!@@"
 
 _BikeShopComeAgainText::
-IF DEF(_YELLOW)
-	text "Come back again"
-	line "sometime!"
+	text $02,$D9,$07,$8D,$07,$01,$7F,$03,$97,$7F,$07,$20,$03,$F5,$0B,$66; RAW DATA : 	text "다음에 또 오렴!"
 	done
-ELSE
-	text "Come back again"
-	line "some time!"
-	done
-
-ENDC
-
 
 _BikeShopText_1d82f::
-	text "How do you like"
-	line "your new BICYCLE?"
+	text $05,$C5,$7F,$07,$AA,$07,$CC,$01,$25,$02,$C2; RAW DATA : 	text "새 자전거는"
+	line $04,$46,$07,$8D,$07,$01,$7F,$03,$45,$02,$CF,$0B,$67; RAW DATA : 	line "마음에 드니?"
 
-	para "You can take it"
-	line "on CYCLING ROAD"
-	cont "and in caves!"
+	para $06,$6E,$07,$9C,$09,$7C,$04,$45,$7F,$03,$FE,$03,$45,$07,$01,$05,$DD; RAW DATA : 	para "싸이클링 로드에서"
+	line $09,$88,$01,$4D,$7F,$02,$DE,$04,$41,$06,$26,$7F,$07,$A6,$01,$4D; RAW DATA : 	line "타고 달릴수 있고"
+	cont $03,$1F,$01,$8C,$07,$01,$05,$DD,$03,$15,$7F,$01,$01,$02,$C9,$0A,$6F,$02,$DC,$02,$D9; RAW DATA : 	cont "동굴에서도 가능하단다"
 	done
 
 _BikeShopText_1d834::
-	text "You better make"
-	line "room for this!"
+	text $07,$9C,$01,$29,$03,$FE,$7F,$02,$77,$7F,$04,$E6,$07,$9C; RAW DATA : 	text "이걸로 네 방이"
+	line $07,$EB,$02,$F5,$7F,$07,$F1,$06,$C6,$08,$2A,$01,$25,$03,$C5,$02,$D9; RAW DATA : 	line "좀더 좋아질거란다"
 	done
 
 _BikeShopText_1d843::
-	text "A plain city BIKE"
-	line "is good enough"
-	cont "for me!"
+	text $02,$3A,$0A,$71,$09,$A7,$02,$C2,$7F,$06,$63,$02,$4B,$07,$4B,$7F,$07,$AA,$07,$CC,$01,$25; RAW DATA : 	text "나한테는 시내용 자전거"
+	line $07,$D4,$03,$15,$04,$89,$7F,$08,$E6,$05,$30,$0A,$78; RAW DATA : 	line "정도면 충분해"
 
-	para "You can't put a"
-	line "shopping basket"
-	cont "on an MTB!"
+	para $05,$BA,$06,$C7,$07,$4B,$7F,$07,$AA,$07,$CC,$01,$25,$07,$01,$02,$C2,$7F,$07,$B5,$04,$D9,$01,$88,$02,$CF,$04,$36; RAW DATA : 	para "산악용 자전거에는 장바구니를"
+	line $02,$DE,$7F,$06,$26,$01,$01,$7F,$06,$F8,$07,$88,$02,$CF,$01,$C1; RAW DATA : 	line "달 수가 없으니깐"
 	done
 
 _BikeShopText_1d85c::
-	text "These BIKEs are"
-	line "cool, but they're"
-	cont "way expensive!"
+	text $07,$CA,$7F,$07,$AA,$07,$CC,$01,$25,$03,$49; RAW DATA : 	text "저 자전거들"
+	line $04,$7A,$07,$DE,$05,$18,$07,$09; RAW DATA : 	line "멋져보여"
+	cont $01,$A7,$03,$E1,$03,$05,$7F,$07,$FB,$02,$D9,$7F,$05,$61,$06,$6E,$0B,$6A,$0B,$6A; RAW DATA : 	cont "그런데 죄다 비싸……"
 	done
 
 _BikeShopText_1d861::
-	text "Wow. Your BIKE is"
-	line "really cool!"
+	text $07,$2D,$06,$C6,$0B,$68,$7F,$02,$77,$7F,$07,$AA,$07,$CC,$01,$25; RAW DATA : 	text "와아- 네 자전거"
+	line $07,$D4,$04,$4B,$7F,$04,$7A,$07,$A6,$06,$EE; RAW DATA : 	line "정말 멋있어"
 	done
