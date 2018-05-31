d6b2a3cd93a80441000cdf2f87dc3fe77da9fbb1
WindowsTiger <0xodbs02@naver.com>
2018-05-31 13:28:05 +0900

    Update celadon_mansion_1f.asm

--- text/maps/celadon_mansion_1f.asm
+++ text/maps/celadon_mansion_1f.asm
@@ -1,69 +1,62 @@
 _CeladonMansion1Text1::
-	text "MEOWTH: Meow!@@"
+	text $02,$3A,$07,$2B,$0B,$62,$02,$3A,$0B,$68,$07,$2B,$0B,$66,$50,$50; RAW DATA : 	text "나옹『나-옹!@@"
 
 _CeladonMansion1Text2::
-	text "My dear #MON"
-	line "keep me company."
+	text $0A,$27,$09,$2F,$04,$93,$07,$01,$7F,$03,$31,$03,$DF,$06,$6E,$07,$09,$7F,$07,$A6,$07,$88,$04,$89; RAW DATA : 	text "포켓몬에 둘러싸여 있으면"
+	line $0A,$A5,$07,$AA,$7F,$05,$BC,$06,$C6,$03,$15,$0B,$6A,$0B,$6A; RAW DATA : 	line "혼자 살아도……"
+	cont $06,$B5,$06,$B5,$0A,$6F,$08,$26,$7F,$06,$CA,$06,$C6,$0B,$6A,$0B,$6A; RAW DATA : 	cont "쓸쓸하지 않아……"
 
-	para "MEOWTH even brings"
-	line "money home!"
+	para $07,$4C,$04,$3E,$08,$2D,$7F,$02,$3A,$07,$2B,$07,$9C,$02,$C2,$0B,$6A,$0B,$6A; RAW DATA : 	para "우리집 나옹이는……"
+	line $01,$01,$02,$2B,$06,$BF,$7F,$03,$17,$07,$8B,$7F,$08,$06,$07,$66,$07,$22,$02,$DC,$02,$D9,$0B,$66; RAW DATA : 	line "가끔씩 돈을 주워온단다!"
 	done
 
 _CeladonMansion1Text3::
-	text "CLEFAIRY: Pi"
-	line "pippippi!@@"
+	text $05,$AF,$05,$AF,$0B,$62,$05,$AF,$7F,$05,$AF,$05,$AF,$0B,$66,$50,$50; RAW DATA : 	text "삐삐『삐 삐삐!@@"
 
 _CeladonMansion1Text4::
-	text "NIDORAN: Kya"
-	line "kyaoo!@@"
+	text $02,$CF,$03,$45,$03,$E1,$0B,$62,$02,$CF,$03,$45,$7F,$02,$CF,$03,$45,$03,$E1,$0B,$66,$50,$50; RAW DATA : 	text "니드런『니드 니드런!@@"
 
 _CeladonMansion1Text5::
-	text "CELADON MANSION"
-	line "Manager's Suite"
+	text $04,$AB,$08,$26,$01,$13,$7F,$04,$67,$05,$F7; RAW DATA : 	text "무지개 맨션"
+	line $0B,$6A,$0B,$6A,$7F,$01,$6C,$04,$3E,$07,$9E,$06,$67; RAW DATA : 	line "…… 관리인실"
 	done
 
 IF DEF(_YELLOW)
-
 _CeladonMansion1Text6::
-	text "Oh, you have an"
-	line "adorable PIKACHU"
-	cont "with you.@@"
+	text $07,$20,$7F,$01,$82,$07,$B5,$0A,$F7,$7F,$05,$B7,$03,$CB,$06,$4A,$03,$E1; RAW DATA : 	text "오 굉장히 사랑스런"
+	line $0A,$67,$09,$0B,$08,$F2,$03,$CB,$7F,$07,$A6,$01,$88,$02,$3A; RAW DATA : 	line "피카츄랑 있구나"
+	done
 
 _CeladonMansion1Text7::
-	text "It seems like it"
-	line "hasn't been tamed"
-	cont "at all."
+	text $02,$6E,$7F,$01,$A7,$02,$D9,$08,$26,$7F,$03,$6B,$04,$33,$08,$26; RAW DATA : 	text "널 그다지 따르지"
+	line $06,$CA,$02,$C2,$01,$2D,$08,$B3,$03,$E3,$7F,$05,$18,$07,$9C,$02,$C2,$01,$88,$02,$3A; RAW DATA : 	line "않는것처럼 보이는구나"
 	done
 
 _CeladonMansion1Text8::
-	text "Why don't you"
-	line "take more care"
-	cont "with PIKACHU?"
+	text $02,$77,$7F,$0A,$67,$09,$0B,$08,$F2,$0A,$71,$09,$A7,$7F,$07,$EB,$02,$F5; RAW DATA : 	text "네 피카츄한테 좀더"
+	line $01,$6C,$06,$69,$07,$8B,$7F,$01,$01,$07,$DE,$08,$06,$04,$89,$7F,$06,$EE,$03,$81,$01,$3A,$02,$CF,$0B,$67; RAW DATA : 	line "관심을 가져주면 어떡겠니?"
 	done
 
 _CeladonMansion1Text9::
-	text "You must be happy"
-	line "to have a #MON"
-	cont "that cute."
+	text $0A,$80,$05,$19,$0A,$71,$01,$34,$7F,$09,$E2,$04,$42,$06,$F8,$01,$88,$02,$3A; RAW DATA : 	text "행복한게 틀림없구나"
+	line $01,$9D,$07,$09,$07,$4E,$7F,$0A,$27,$09,$2F,$04,$93,$01,$6A,$7F,$0A,$74,$01,$E2,$0A,$78,$05,$DD; RAW DATA : 	line "귀여운 포켓몬과 함께해서"
 	done
 
-
 _CeladonMansion1Text10::
-	text "Your PIKACHU seems"
-	line "tamed."
+	text $02,$77,$7F,$0A,$67,$09,$0B,$08,$F2,$02,$C2; RAW DATA : 	text "네 피카츄는"
+	line $02,$6A,$04,$36,$7F,$07,$AF,$7F,$03,$6B,$04,$33,$01,$4D,$7F,$07,$A6,$01,$88,$02,$3A; RAW DATA : 	line "너를 잘 따르고 있구나"
 	done
 
 _CeladonMansion1Text11::
-	text "Your PIKACHU looks"
-	line "happy with you."
+	text $02,$77,$7F,$0A,$67,$09,$0B,$08,$F2,$02,$C2,$7F,$02,$6A,$0A,$6F,$01,$4D,$7F,$07,$A6,$02,$C2,$01,$34; RAW DATA : 	text "네 피카츄는 너하고 있는게"
+	line $0A,$80,$05,$19,$0A,$78,$7F,$05,$18,$07,$9C,$02,$C2,$01,$88,$02,$3A; RAW DATA : 	line "행복해 보이는구나"
 	done
 
 _CeladonMansion1Text12::
-	text "You look like a"
-	line "fantastic duo."
+	text $0A,$AF,$05,$C3,$07,$97,$7F,$03,$40,$07,$20,$08,$B3,$03,$E3; RAW DATA : 	text "환상의 듀오처럼"
+	line $05,$18,$07,$9C,$02,$C2,$01,$88,$02,$3A; RAW DATA : 	line "보이는구나"
 
-	para "You're making me"
-	line "jealous!"
+	para $02,$4B,$01,$01,$7F,$08,$2A,$09,$C5,$01,$01; RAW DATA : 	para "내가 질투가"
+	line $02,$3F,$7F,$07,$D4,$03,$15,$03,$FE,$01,$88,$02,$3A,$0B,$66; RAW DATA : 	line "날 정도로구나!"
 	done
-
 ENDC
