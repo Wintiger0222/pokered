7bf2cf02d9da826e851b0fb98a9b991a1c15600c
WindowsTiger <0xodbs02@naver.com>
2018-05-31 13:15:38 +0900

    Update bills_house.asm

--- text/maps/bills_house.asm
+++ text/maps/bills_house.asm
@@ -1,94 +1,94 @@
 IF DEF(_YELLOW)
 _BillsHouseDontLeaveText::
-	text "Whoa, don't go"
-	line "anywhere, wait!"
+	text $06,$EE,$06,$F9,$0B,$68; RAW DATA : 	text "어엇-"
+	line $01,$01,$08,$26,$04,$46,$7F,$01,$B2,$02,$D9,$03,$F1,$0B,$68,$0B,$66; RAW DATA : 	line "가지마 기다려-!"
 	done
 ENDC
-
 _BillsHouseText_1e865::
-	text "Hiya! I'm a"
-	line "#MON..."
-	cont "...No I'm not!"
+	text $09,$79,$0A,$EC; RAW DATA : 	text "크흠"
+	line $02,$3A,$02,$C2,$7F,$0A,$27,$09,$2F,$04,$93,$0B,$6A,$0B,$6A; RAW DATA : 	line "나는 포켓몬……"
+	cont $0B,$6A,$0B,$6A,$07,$9C,$7F,$06,$C6,$02,$CF,$03,$C3,$0B,$66; RAW DATA : 	cont "……이 아니라!"
 
-	para "Call me BILL!"
-	line "I'm a true blue"
-	cont "#MANIAC! Hey!"
-	cont "What's with that"
-	cont "skeptical look?"
+	para $07,$9C,$06,$26,$07,$B7,$03,$C3,$01,$4D,$7F,$0A,$78; RAW DATA : 	para "이수재라고 해"
+	line $02,$3A,$06,$DF,$04,$4B,$03,$FE,$7F,$08,$28,$07,$D4,$0A,$71; RAW DATA : 	line "나야말로 진정한"
+	cont $0A,$27,$09,$2F,$04,$93,$01,$7B,$08,$35,$0B,$66; RAW DATA : 	cont "포켓몬괴짜!"
+	
+	para $06,$EE,$07,$9C,$0B,$66,$7F,$04,$BB,$7F,$01,$A7,$03,$E8,$01,$34,$7F,$07,$97,$06,$69,$06,$4A,$03,$DF,$07,$4E; RAW DATA : 	para "어이! 뭘 그렇게 의심스러운"
+	line $02,$AB,$08,$CA,$04,$3E,$03,$FE,$7F,$05,$18,$01,$4D,$7F,$01,$A7,$03,$D1,$0B,$67; RAW DATA : 	line "눈초리로 보고 그래?"
 
-	para "I'm not joshing"
-	line "you, I screwed up"
-	cont "an experiment and"
-	cont "got combined with"
-	cont "a #MON!"
+	para $07,$B5,$02,$3D,$09,$01,$02,$C2,$01,$25,$7F,$06,$C6,$02,$CF,$03,$D1,$03,$15; RAW DATA : 	para "장난치는거 아니래도"
+	line $02,$3D,$7F,$06,$67,$0A,$88,$0A,$6F,$02,$D9,$01,$01; RAW DATA : 	line "난 실험하다가"
+	cont $05,$B7,$01,$4D,$07,$01,$7F,$0A,$D6,$04,$4B,$03,$F1,$05,$DD; RAW DATA : 	cont "사고에 휘말려서"
+	cont $0A,$27,$09,$2F,$04,$93,$07,$9C,$03,$CB; RAW DATA : 	cont "포켓몬이랑"
+	cont $0A,$75,$08,$C4,$08,$28,$01,$25,$03,$C3,$02,$CF,$01,$C1,$0B,$66; RAW DATA : 	cont "합쳐진거라니깐!"
 
-	para "So, how about it?"
-	line "Help me out here!"
+	para $07,$9C,$07,$D6,$7F,$06,$EE,$03,$81,$01,$34,$7F,$0A,$78,$06,$DF,$01,$3A,$06,$EE,$0B,$67; RAW DATA : 	para "이제 어떡게 해야겠어?"
+	line $07,$9C,$7F,$05,$C3,$0A,$B2,$07,$01,$05,$DD,$7F,$04,$FE,$06,$EE,$02,$3A,$01,$34,$7F,$03,$15,$07,$2D,$08,$10; RAW DATA : 	line "이 상황에서 벗어나게 도와줘"
 	done
 
 _BillsHouseText_1e86a::
-	text "When I'm in the"
-	line "TELEPORTER, go to"
-	cont "my PC and run the"
-	cont "Cell Separation"
-	cont "System!"
+	text $02,$4B,$01,$01,$7F,$07,$CC,$06,$0B,$07,$B5,$09,$01,$07,$01,$7F,$03,$49,$06,$EE,$01,$01,$04,$89; RAW DATA : 	text "내가 전송장치에 들어가면"
+	line $02,$6A,$02,$C2,$7F,$02,$4B,$7F,$09,$24,$0A,$4B,$09,$9D,$03,$FE,$7F,$01,$01,$05,$DD; RAW DATA : 	line "너는 내 컴퓨터로 가서"
+	cont $05,$EC,$0A,$27,$7F,$05,$30,$04,$3E,$7F,$06,$63,$06,$4A,$09,$AB,$07,$8B; RAW DATA : 	cont "세포 분리 시스템을"
+	cont $07,$AB,$03,$1F,$06,$63,$09,$31,$08,$11,$07,$88,$04,$89,$7F,$0A,$78; RAW DATA : 	cont "작동시켜줬으면 해"
+	cont $05,$2E,$09,$89,$07,$9C,$06,$DF,$0B,$66; RAW DATA : 	cont "부탁이야!"
 	done
 
 _BillsHouseText_1e86f::
-	text "No!? Come on, you"
-	line "gotta help a guy"
-	cont "in deep trouble!"
+	text $06,$C8,$03,$25,$0B,$66,$0B,$66,$7F,$07,$D6,$04,$DF,$7F,$07,$EB,$0B,$66; RAW DATA : 	text "안돼!! 제발 좀!"
+	line $06,$69,$01,$02,$0A,$71,$7F,$04,$AE,$07,$D6,$07,$01,$7F,$05,$6C,$08,$28; RAW DATA : 	line "심각한 문제에 빠진"
+	cont $05,$B7,$03,$C7,$07,$8A,$7F,$03,$15,$07,$2D,$06,$DF,$7F,$0A,$71,$02,$D9,$01,$4D,$0B,$66; RAW DATA : 	cont "사람은 도와야 한다고!"
 
-	para "What do you say,"
-	line "chief? Please?"
-	cont "OK? All right!"
+	para $04,$B9,$03,$C3,$01,$4D,$7F,$05,$32,$03,$DF,$08,$10,$0B,$67; RAW DATA : 	para "뭐라고 불러줘?"
+	line $0A,$9C,$02,$D4,$0B,$67,$7F,$07,$D6,$04,$DF,$7F,$07,$EB,$07,$44,$0B,$66; RAW DATA : 	line "형님? 제발 좀요!"
+	cont $03,$26,$08,$26,$0B,$67,$7F,$07,$F1,$06,$D2,$06,$EE,$0B,$66; RAW DATA : 	cont "됐지? 좋았어!"
 	prompt
 
 _BillThankYouText::
-	text "BILL: Yeehah!"
-	line "Thanks, bud! I"
-	cont "owe you one!"
+	text $07,$9C,$06,$26,$07,$B7,$0B,$62,$07,$9C,$01,$25,$7F,$01,$4D,$04,$46,$07,$4E,$01,$29; RAW DATA : 	text "이수재『이거 고마운걸"
+	line $09,$DE,$03,$E9,$07,$9C,$02,$6A; RAW DATA : 	line "트레이너"
+	cont $05,$6A,$07,$8B,$7F,$07,$E3,$06,$EE; RAW DATA : 	cont "빚을 졌어"
 
-	para "So, did you come"
-	line "to see my #MON"
-	cont "collection?"
-	cont "You didn't?"
-	cont "That's a bummer."
+	para $02,$4B,$7F,$0A,$27,$09,$2F,$04,$93,$7F,$09,$23,$03,$EA,$05,$F7; RAW DATA : 	para "내 포켓몬 컬렉션"
+	line $05,$18,$03,$DF,$7F,$07,$22,$01,$25,$08,$26,$0B,$67; RAW DATA : 	line "보러 온거지?"
+	cont $06,$C6,$02,$CF,$03,$C3,$01,$4D,$0B,$67; RAW DATA : 	cont "아니라고?"
+	cont $07,$9C,$01,$25,$7F,$06,$67,$04,$61,$07,$9C,$02,$77; RAW DATA : 	cont "이거 실망이네"
 
-	para "I've got to thank"
-	line "you... Oh here,"
-	cont "maybe this'll do."
+	para $04,$BA,$01,$01,$7F,$05,$18,$02,$E4,$07,$8B,$7F,$0A,$78,$06,$DF,$0A,$72,$09,$A9,$03,$05; RAW DATA : 	para "뭔가 보답을 해야할텐데"
+	line $0B,$6A,$0B,$6A,$06,$C6,$0B,$66,$7F,$07,$9C,$01,$34,$7F,$07,$F1,$01,$3A,$02,$77; RAW DATA : 	line "……아! 이게 좋겠네"
+	cont $07,$09,$01,$B2,$07,$A6,$06,$EE; RAW DATA : 	cont "여기있어"
 	prompt
 
 _SSTicketReceivedText::
-	text "<PLAYER> received"
-	line "an @"
+	text $52, $02,$C2,$0B,$64,$07,$8A,$0B,$65; RAW DATA : 	text $52, "는(은)"
+	line $50; RAW DATA : 	line "@"
 	TX_RAM wcf4b
-	text "!@@"
+	text $04,$36,$0B,$64,$07,$8B,$0B,$65; RAW DATA : 	text "를(을)"
+	cont $06,$F2,$06,$FA,$02,$D9,$0B,$66,$0B,$66,$50,$50; RAW DATA : 	cont "얻었다!!@@"
 
 _SSTicketNoRoomText::
-	text "You've got too"
-	line "much stuff, bud!"
+	text $01,$A7,$7F,$07,$9C,$05,$C3,$07,$8A; RAW DATA : 	text "그 이상은"
+	line $08,$26,$02,$D2,$7F,$06,$26,$7F,$06,$F8,$06,$EE,$7F,$09,$DE,$03,$E9,$07,$9C,$02,$6A; RAW DATA : 	line "지닐 수 없어 트레이너"
 	done
 
 _BillsHouseText_1e8cb::
-	text "That cruise ship,"
-	line "S.S.ANNE, is in"
-	cont "VERMILION CITY."
-	cont "Its passengers"
-	cont "are all trainers!"
+	text $01,$05,$05,$C6,$06,$63,$09,$EC,$07,$01,$7F,$05,$C3,$09,$DE,$06,$D3,$02,$C0,$0A,$A3,$03,$C3,$02,$C2; RAW DATA : 	text "갈색시티에 상트앙느호라는"
+	line $01,$4D,$01,$AE,$7F,$07,$7F,$03,$C7,$05,$E1,$07,$9C,$7F,$07,$A6,$02,$C2,$03,$05; RAW DATA : 	line "고급 유람선이 있는데"
+	cont $07,$09,$01,$B2,$7F,$06,$62,$01,$14,$03,$49,$07,$8A; RAW DATA : 	cont "여기 승객들은"
+	cont $07,$CC,$05,$2E,$7F,$09,$DE,$03,$E9,$07,$9C,$02,$6A,$03,$C3,$01,$4D,$0B,$66; RAW DATA : 	cont "전부 트레이너라고!"
 
-	para "They invited me"
-	line "to their party,"
-	cont "but I can't stand"
-	cont "fancy do's. Why"
-	cont "don't you go"
-	cont "instead of me?"
+	para $01,$A7,$01,$67,$07,$01,$7F,$01,$A7,$03,$49,$07,$97,$7F,$09,$F4,$09,$EC,$07,$01; RAW DATA : 	para "그곳에 그들의 파티에"
+	line $08,$CA,$02,$EB,$04,$DE,$01,$B4,$7F,$0A,$7F,$08,$26,$04,$48; RAW DATA : 	line "초대받긴 했지만"
+	cont $02,$4B,$01,$01,$7F,$01,$A7,$03,$E1,$01,$67,$0A,$6F,$01,$4D,$02,$C2; RAW DATA : 	cont "내가 그런곳하고는"
+	cont $07,$15,$7F,$04,$62,$08,$26,$7F,$06,$CA,$01,$25,$03,$47; RAW DATA : 	cont "영 맞지 않거든"
+	
+	para $01,$76,$08,$9A,$02,$D9,$04,$89; RAW DATA : 	para "괜찮다면"
+	line $02,$3A,$7F,$02,$EB,$06,$65,$7F,$01,$01,$08,$09,$03,$D1,$0B,$67; RAW DATA : 	line "나 대신 가줄래?"
 	done
 
 _BillsHouseText_1e8da::
-	text "BILL: Look, bud,"
-	line "just check out"
-	cont "some of my rare"
-	cont "#MON on my PC!"
+	text $07,$9C,$06,$26,$07,$B7,$0B,$62,$06,$EE,$07,$9C,$7F,$09,$DE,$03,$E9,$07,$9C,$02,$6A; RAW DATA : 	text "이수재『어이 트레이너"
+	line $02,$4B,$7F,$09,$24,$0A,$4B,$09,$9D,$07,$01,$7F,$07,$A6,$02,$C2; RAW DATA : 	line "내 컴퓨터에 있는"
+	cont $0A,$27,$09,$2F,$04,$93,$07,$01,$7F,$02,$EB,$0A,$71,$7F,$0A,$F1,$01,$9D,$07,$AA,$04,$11,$03,$15; RAW DATA : 	cont "포켓몬에 대한 희귀자료도"
+	cont $0A,$AE,$07,$9E,$0A,$78,$05,$18,$03,$C3,$01,$4D; RAW DATA : 	cont "확인해보라고"
 	done
