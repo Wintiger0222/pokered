c9745709b8d61ed43754c05a999686ce10ea0c3e
WindowsTiger <0xodbs02@naver.com>
2018-05-31 13:35:32 +0900

    Update cerulean_city.asm

--- text/maps/cerulean_city.asm
+++ text/maps/cerulean_city.asm
@@ -1,184 +1,161 @@
 _CeruleanCityText_19668::
-	text "<RIVAL>: Yo!"
-	line "<PLAYER>!"
+	text $53, $0B,$62,$05,$20,$08,$06,$04,$33,$0B,$66; RAW DATA : 	text $53, "『봉주르!"
+	line $52, $0B,$66; RAW DATA : 	line $52, "!"
 
-	para "You're still"
-	line "struggling along"
-	cont "back here?"
+	para $02,$6A,$7F,$06,$C6,$08,$27,$03,$15,$7F,$07,$09,$01,$B2,$05,$DD; RAW DATA : 	para "너 아직도 여기서"
+	line $0A,$8C,$04,$7E,$01,$4D,$7F,$07,$A6,$02,$C2,$01,$25,$06,$DF,$0B,$67; RAW DATA : 	line "헤메고 있는거야?"
 
-	para "I'm doing great!"
-	line "I caught a bunch"
-	cont "of strong and"
-	cont "smart #MON!"
+	para $02,$3D,$7F,$04,$FA,$06,$81,$7F,$01,$0D,$0A,$6F,$01,$4D,$7F,$03,$98,$03,$98,$0A,$71,$7F,$0A,$27,$09,$2F,$04,$93,$07,$8B; RAW DATA : 	para "난 벌써 강하고 똑똑한 포켓몬을"
+	line $07,$09,$03,$DF,$04,$46,$04,$3E,$02,$3A,$7F,$07,$B2,$06,$D2,$06,$EE,$0B,$66; RAW DATA : 	line "여러마리나 잡았어!"
 
-	para "Here, let me see"
-	line "what you caught,"
-	cont "<PLAYER>!"
+	para $52, $7F,$02,$6A,$0A,$71,$09,$A7,$03,$15; RAW DATA : 	para $52, " 너한테도"
+	line $02,$4B,$01,$01,$7F,$07,$B2,$07,$8A,$7F,$0A,$27,$09,$2F,$04,$93,$07,$8B,$7F,$05,$18,$07,$09,$08,$09,$01,$34,$0B,$66; RAW DATA : 	line "내가 잡은 포켓몬을 보여줄게!"
 	done
 
 _CeruleanCityText_1966d::
-	text "Hey!"
-	line "Take it easy!"
-	cont "You won already!"
+	text $07,$B1,$01,$C1,$0B,$66,$7F,$08,$28,$07,$D4,$0A,$78,$0B,$66; RAW DATA : 	text "잠깐! 진정해!"
+	line $02,$6A,$01,$01,$7F,$07,$9C,$01,$45,$06,$EE,$0B,$66; RAW DATA : 	line "너가 이겼어!"
 	prompt
 
 _CeruleanCityText_19672::
-	text "Heh!"
-	line "You're no match"
-	cont "for my genius!"
+	text $0A,$8C,$0A,$8C,$0B,$66,$7F,$07,$0A,$06,$63,$7F,$02,$4B,$7F,$08,$B5,$07,$B7,$05,$EA,$7F,$06,$D5,$07,$01,$05,$E1; RAW DATA : 	text "헤헤! 역시 내 천재성 앞에선"
+	line $05,$C3,$02,$EB,$01,$01,$7F,$06,$C8,$03,$27,$02,$C2,$7F,$01,$88,$02,$3A,$0B,$66; RAW DATA : 	line "상대가 안되는 구나!"
 	prompt
 
 _CeruleanCityText_19677::
-	text "<RIVAL>: Hey,"
-	line "guess what?"
+	text $53, $0B,$62,$06,$C6; RAW DATA : 	text $53, "『아"
+	line $01,$A7,$01,$25,$7F,$06,$CB,$06,$C6,$0B,$67; RAW DATA : 	line "그거 알아?"
 
-	para "I went to BILL's"
-	line "and got him to"
-	cont "show me his rare"
-	cont "#MON!"
+	para $02,$4B,$7F,$03,$15,$01,$08,$7F,$0A,$14,$07,$9C,$08,$26; RAW DATA : 	para "내 도감 페이지"
+	line $06,$F6,$08,$BB,$7F,$04,$49,$07,$9C,$7F,$08,$A4,$07,$6C,$02,$D9,$0B,$66; RAW DATA : 	line "엄청 많이 채웠다!"
 
-	para "That added a lot"
-	line "of pages to my"
-	cont "#DEX!"
 
-	para "After all, BILL's"
-	line "world famous as a"
-	cont "#MANIAC!"
+	para $06,$F3,$04,$46,$07,$CC,$07,$01,$7F,$07,$9C,$06,$26,$07,$B7,$0A,$71,$09,$A7,$7F,$01,$01,$05,$DD; RAW DATA : 	para "얼마전에 이수재한테 가서"
+	line $01,$A7,$07,$97,$7F,$0A,$F1,$01,$9D,$7F,$0A,$27,$09,$2F,$04,$93,$07,$8B; RAW DATA : 	line "그의 희귀 포켓몬을"
+	cont $05,$18,$07,$09,$02,$DE,$03,$C3,$7F,$01,$A7,$03,$D8,$01,$25,$03,$47,$0B,$66; RAW DATA : 	cont "보여달라 그랬거든!"
 
-	para "He invented the"
-	line "#MON Storage"
-	cont "System on PC!"
+	para $01,$A7,$01,$01,$7F,$01,$7B,$08,$35,$03,$FE,$7F,$07,$7F,$04,$8D,$0A,$71,$7F,$07,$9C,$07,$7F,$04,$36; RAW DATA : 	para "그가 괴짜로 유명한 이유를"
+	line $06,$CB,$01,$3A,$02,$F5,$03,$C3,$02,$CF,$01,$BE,$0B,$66; RAW DATA : 	line "알겠더라니까!"
 
-	para "Since you're using"
-	line "his system, go"
-	cont "thank him!"
+	para $01,$34,$02,$D9,$01,$01,$7F,$09,$24,$0A,$4B,$09,$9D,$07,$97; RAW DATA : 	para "게다가 컴퓨터의"
+	line $0A,$27,$09,$2F,$04,$93,$7F,$04,$DA,$06,$4A,$7F,$06,$63,$06,$4A,$09,$AB,$07,$01,$03,$15; RAW DATA : 	line "포켓몬 박스 시스템에도"
+	cont $08,$CA,$02,$EB,$0A,$78,$08,$11,$06,$EE,$0B,$66; RAW DATA : 	cont "초대해줬어!"
 
-	para "Well, I better"
-	line "get rolling!"
-	cont "Smell ya later!"
+	para $02,$6A,$03,$15,$7F,$01,$A7,$7F,$06,$63,$06,$4A,$09,$AB,$07,$8B,$7F,$06,$B2,$01,$4D,$7F,$07,$A6,$07,$88,$02,$CF,$01,$BE; RAW DATA : 	para "너도 그 시스템을 쓰고 있으니까"
+	line $0A,$71,$04,$F8,$7F,$07,$9C,$06,$DF,$01,$B2,$7F,$02,$3A,$02,$A9,$06,$EE,$05,$21,$0B,$66; RAW DATA : 	line "한번 이야기 나누어봐!"
+
+	para $07,$9C,$07,$D6,$7F,$01,$A7,$04,$48,$7F,$01,$01,$05,$21,$06,$DF,$7F,$01,$3A,$02,$D9; RAW DATA : 	para "이제 그만 가봐야 겠다"
+	line $01,$A7,$03,$E3,$7F,$04,$D9,$07,$9C,$05,$61,$0B,$66; RAW DATA : 	line "그럼 바이비!"
 	done
 
 _CeruleanCityText_196d9::
-	text "Hey! Stay out!"
-	line "It's not your"
-	cont "yard! Huh? Me?"
+	text $07,$B1,$01,$C1,$7F,$01,$25,$01,$B2,$0B,$66,$7F,$02,$3A,$01,$01,$03,$C3,$01,$4D,$0B,$66; RAW DATA : 	text "잠깐 거기! 나가라고!"
+	line $07,$09,$01,$B2,$02,$C2,$7F,$02,$4B,$7F,$03,$75,$07,$9C,$06,$DF,$0B,$66; RAW DATA : 	line "여기는 내 땅이야!"
+	cont $07,$90,$0B,$67,$7F,$02,$3A,$0B,$67; RAW DATA : 	cont "응? 나?"
 
-	para "I'm an innocent"
-	line "bystander! Don't"
-	cont "you believe me?"
+	para $02,$3D,$7F,$01,$A7,$02,$69,$7F,$08,$26,$02,$3A,$01,$01,$02,$F8,$7F,$0A,$80,$07,$9E,$07,$9C,$03,$C3,$01,$88,$0B,$66; RAW DATA : 	para "난 그냥 지나가던 행인이라구!"
+	line $08,$28,$08,$35,$06,$DF,$0B,$66; RAW DATA : 	line "진짜야!"
 	done
 
 _ReceivedTM28Text::
-	text "<PLAYER> recovered"
-	line "TM28!@@"
+	text $52, $02,$C2,$0B,$64,$07,$8A,$0B,$65; RAW DATA : 	text $52, "는(은)"
+	line $01,$B2,$06,$2A,$04,$73,$06,$65,$F8,$FE,$07,$8B,$7F,$03,$19,$03,$F1,$04,$DE,$06,$D2,$02,$D9,$0B,$66,$50,$50; RAW DATA : 	line "기술머신28을 돌려받았다!@@"
 
 _ReceivedTM28Text2::
 	db $0
-	para "I better get"
-	line "moving! Bye!@@"
+	para $07,$9C,$07,$D6,$7F,$02,$3A,$02,$C2,$7F,$01,$03,$02,$D9,$0B,$66; RAW DATA : 	para "이제 나는 간다!"
+	line $06,$C8,$02,$87,$0B,$66,$50,$50; RAW DATA : 	line "안녕!@@"
 
 _TM28NoRoomText::
-	text "Make room for"
-	line "this!"
+	text $07,$9C,$01,$29,$7F,$02,$76,$07,$8B,$7F,$01,$68,$01,$03,$07,$8B,$7F,$04,$48,$03,$49,$06,$EE,$08,$10,$0B,$66; RAW DATA : 	text "이걸 넣을 공간을 만들어줘!"
 
-	para "I can't run until"
-	line "I give it to you!"
+	para $07,$9C,$01,$29,$7F,$08,$06,$01,$B2,$7F,$07,$CC,$07,$03; RAW DATA : 	para "이걸 주기 전엔"
+	line $02,$6A,$01,$01,$7F,$01,$48,$06,$03,$7F,$03,$6B,$03,$C3,$07,$23,$09,$A7,$02,$CF,$01,$BE,$0B,$66; RAW DATA : 	line "너가 계속 따라올테니까!"
 	done
 
 _CeruleanCityText_196ee::
-	text "Stop!"
-	line "I give up! I'll"
-	cont "leave quietly!"
+	text $07,$B1,$01,$C1,$0B,$66; RAW DATA : 	text "잠깐!"
+	line $0A,$77,$05,$19,$0A,$72,$01,$E2,$0B,$66; RAW DATA : 	line "항복할께!"
+	cont $07,$E6,$07,$4B,$0A,$F7,$7F,$02,$3A,$01,$05,$7F,$09,$A7,$02,$CF,$01,$BE,$0B,$66; RAW DATA : 	cont "조용히 나갈 테니까!"
 	prompt
 
 _CeruleanCityText_196f3::
-	text "OK! I'll return"
-	line "the TM I stole!"
+	text $06,$CB,$01,$3A,$06,$EE,$0B,$66,$7F,$02,$4B,$01,$01,$7F,$0A,$C9,$09,$03; RAW DATA : 	text "알겠어! 내가 훔친"
+	line $01,$B2,$06,$2A,$04,$73,$06,$65,$03,$15,$7F,$03,$19,$03,$F1,$08,$09,$01,$34,$0B,$66; RAW DATA : 	line "기술머신도 돌려줄게!"
 	prompt
 
 _CeruleanCityText3::
-	text "You're a trainer"
-	line "too? Collecting,"
-	cont "fighting, it's a"
-	cont "tough life."
+	text $02,$6A,$03,$15,$7F,$09,$DE,$03,$E9,$07,$9C,$02,$6A,$06,$DF,$0B,$67; RAW DATA : 	text "너도 트레이너야?"
+	line $07,$B2,$01,$4D,$7F,$06,$6E,$07,$4C,$01,$4D; RAW DATA : 	line "잡고 싸우고"
+	cont $01,$4D,$02,$DE,$0A,$62,$7F,$05,$BE,$07,$9C,$08,$26; RAW DATA : 	cont "고달픈 삶이지"
 	done
 
-_CeruleanCityText4::
-	text "That bush in"
-	line "front of the shop"
-	cont "is in the way."
 
-	para "There might be a"
-	line "way around."
+_CeruleanCityText4::
+	text $0A,$61,$03,$EB,$03,$49,$04,$3E,$06,$23,$7F,$06,$D5,$07,$01,$7F,$07,$A6,$02,$C2,$7F,$02,$3A,$04,$AB,$01,$01; RAW DATA : 	text "프렌들리숍 앞에 있는 나무가"
+	line $01,$B6,$07,$8B,$7F,$04,$47,$01,$4D,$7F,$07,$A6,$06,$EE; RAW DATA : 	line "길을 막고 있어"
+
+	para $06,$C6,$04,$46,$7F,$03,$19,$06,$C6,$7F,$01,$05,$7F,$06,$26,$7F,$07,$A6,$02,$C2; RAW DATA : 	para "아마 돌아 갈 수 있는"
+	line $02,$D9,$04,$35,$7F,$01,$B6,$07,$9C,$7F,$07,$A6,$07,$8B,$01,$25,$06,$DF; RAW DATA : 	line "다른 길이 있을거야"
 	done
 
 _CeruleanCityText5::
-	text "You're making an"
-	line "encyclopedia on"
-	cont "#MON? That"
-	cont "sounds amusing."
+	text $0A,$27,$09,$2F,$04,$93,$7F,$03,$15,$01,$08,$07,$8B,$7F,$04,$48,$03,$47,$02,$D9,$01,$4D,$0B,$67; RAW DATA : 	text "포켓몬 도감을 만든다고?"
+	line $01,$A7,$01,$25,$7F,$02,$EB,$02,$DC,$0A,$71,$03,$05,$0B,$66; RAW DATA : 	line "그거 대단한데!"
 	done
 IF DEF(_YELLOW)
 _CeruleanCityText6::
-	text "These poor people"
-	line "here were robbed."
+	text $07,$09,$01,$B2,$7F,$07,$9C,$7F,$05,$B7,$03,$C7,$03,$49,$07,$8A; RAW DATA : 	text "여기 이 사람들은"
+	line $05,$63,$08,$2D,$09,$A0,$07,$9C,$04,$36,$7F,$02,$E7,$0A,$7F,$02,$DC,$02,$D9; RAW DATA : 	line "빈집털이를 당했단다"
 
-	para "We're positive"
-	line "that TEAM ROCKET"
-	cont "is behind this"
-	cont "terrible deed."
+	para $07,$4C,$04,$3E,$02,$C2,$7F,$07,$9C,$7F,$05,$B7,$01,$27,$07,$97,$7F,$04,$E8,$0A,$C4,$07,$01,$02,$C2; RAW DATA : 	para "우리는 이 사건의 배후에는"
+	line $03,$FE,$09,$2F,$02,$DC,$07,$9C,$7F,$07,$A6,$02,$D9,$01,$4D,$7F,$0A,$AE,$06,$65,$0A,$78; RAW DATA : 	line "로켓단이 있다고 확신해"
 
-	para "Even our POLICE"
-	line "FORCE has trouble"
-	cont "with the ROCKETs!"
+	para $03,$FE,$09,$2F,$02,$DC,$7F,$03,$77,$04,$AE,$07,$01; RAW DATA : 	para "로켓단 때문에"
+	line $01,$46,$08,$9B,$03,$49,$03,$15,$7F,$01,$61,$04,$73,$04,$3E,$7F,$06,$82,$02,$C2,$02,$D9,$02,$CF,$01,$BE,$0B,$66; RAW DATA : 	line "경찰들도 골머리 썩는다니까!"
 	done
 
+ 
+
 _CeruleanCityText_19730::
-	text "OK! ELECTRODE!"
-	line "Use SONICBOOM!"
-	cont "Please ELECTRODE,"
-	cont "pay attention!"
+	text $07,$AA,$7F,$05,$35,$05,$1C,$0B,$66; RAW DATA : 	text "자 붐볼!"
+	line $07,$10,$03,$1F,$03,$F2,$07,$8B,$7F,$04,$DF,$0A,$D6,$0A,$78,$03,$C3,$0B,$66; RAW DATA : 	line "염동력을 발휘해라!"
+	cont $05,$35,$05,$1C,$0B,$66; RAW DATA : 	cont "붐볼!"
+	cont $07,$D6,$04,$DF,$7F,$08,$2D,$08,$0F,$7F,$07,$EB,$7F,$0A,$78,$0B,$66; RAW DATA : 	cont "제발 집중 좀 해!"
 	done
 
 _CeruleanCityText_19735::
-	text "ELECTRODE, TACKLE!"
-	line "No! You blew it"
-	cont "again!"
+	text $05,$35,$05,$1C,$0B,$66,$7F,$04,$96,$09,$BB,$04,$DA,$09,$01,$01,$B2,$0B,$66; RAW DATA : 	text "붐볼! 몸통박치기!"
+	line $06,$C6,$02,$CF,$06,$DF,$0B,$66,$7F,$03,$97,$7F,$06,$67,$06,$26,$0A,$7F,$07,$AD,$06,$C6,$0B,$66; RAW DATA : 	line "아니야! 또 실수했잖아!"
 	done
 
 _CeruleanCityText_1973a::
-	text "ELECTRODE, SWIFT!"
-	line "No! That's wrong!"
+	text $05,$35,$05,$1C,$0B,$66,$7F,$06,$4A,$0A,$67,$03,$45,$7F,$06,$4A,$09,$88,$0B,$66; RAW DATA : 	text "붐볼! 스피드 스타!"
+	line $06,$C6,$02,$CF,$0B,$66,$7F,$06,$C6,$02,$CF,$03,$C3,$02,$CF,$01,$BE,$0B,$66; RAW DATA : 	line "아니! 아니라니까!"
 
-	para "Training #MON"
-	line "is difficult!"
+	para $0A,$27,$09,$2F,$04,$93,$07,$8B,$7F,$0A,$C6,$03,$F3,$06,$63,$09,$80,$02,$C2,$01,$27; RAW DATA : 	para "포켓몬을 훈련시키는건"
+	line $06,$EE,$03,$F1,$07,$66,$0B,$66; RAW DATA : 	line "어려워!"
 
-	para "Your #MON's"
-	line "obedience depends"
-	cont "on your abilities"
-	cont "as a trainer!"
+	para $02,$77,$7F,$0A,$27,$09,$2F,$04,$93,$07,$9C,$7F,$02,$6E,$7F,$06,$F3,$04,$46,$02,$3A; RAW DATA : 	para "네 포켓몬이 널 얼마나"
+	line $07,$AF,$7F,$03,$6B,$04,$36,$08,$26,$02,$C2,$7F,$02,$6A,$07,$97; RAW DATA : 	line "잘 따를지는 너의"
+	cont $09,$DE,$03,$E9,$07,$9C,$02,$6A,$03,$FE,$05,$DD,$07,$97,$7F,$02,$C9,$03,$F2,$07,$01,$7F,$02,$DE,$03,$F1,$07,$A6,$06,$EE,$0B,$66; RAW DATA : 	cont "트레이너로서의 능력에 달려있어!"
 	done
 
 _CeruleanCityText_1976f::
-	text "ELECTRODE took a"
-	line "snooze..."
+	text $05,$35,$05,$1C,$07,$8A,$7F,$07,$E9,$01,$4D,$07,$A6,$02,$D9,$0B,$6A,$0B,$6A; RAW DATA : 	text "붐볼은 졸고있다……"
 	done
 
 _CeruleanCityText_19774::
-	text "ELECTRODE is"
-	line "loafing around..."
+	text $05,$35,$05,$1C,$07,$8A,$7F,$01,$8C,$03,$DF,$02,$D9,$02,$CF,$01,$4D,$07,$A6,$02,$D9,$0B,$6A,$0B,$6A; RAW DATA : 	text "붐볼은 굴러다니고있다……"
 	done
 
 _CeruleanCityText_19779::
-	text "ELECTRODE turned"
-	line "away..."
+	text $05,$35,$05,$1C,$07,$8A,$7F,$03,$3A,$03,$19,$06,$D2,$02,$D9,$0B,$6A,$0B,$6A; RAW DATA : 	text "붐볼은 뒤돌았다……"
 	done
 
 _CeruleanCityText_1977e::
-	text "ELECTRODE"
-	line "ignored orders..."
+	text $05,$35,$05,$1C,$07,$8A,$7F,$08,$06,$05,$0F,$07,$8B,$7F,$04,$AB,$06,$63,$0A,$6F,$01,$4D,$07,$A6,$02,$D9,$0B,$6A,$0B,$6A; RAW DATA : 	text "붐볼은 주변을 무시하고있다……"
 	done
-
 ELSE
 _CeruleanCityText6::
 	text "The people here"
@@ -242,53 +219,44 @@ _CeruleanCityText_1977e::
 ENDC
 
 _CeruleanCityText9::
-	text "I want a bright"
-	line "red BICYCLE!"
+	text $02,$3D,$7F,$05,$71,$01,$03,$7F,$07,$AA,$07,$CC,$01,$25,$01,$01,$7F,$01,$0E,$01,$4D,$06,$6D,$06,$EE,$0B,$66; RAW DATA : 	text "난 빨간 자전거가 갖고싶어!"
 
-	para "I'll keep it at"
-	line "home, so it won't"
-	cont "get dirty!"
+	para $02,$F5,$03,$DF,$07,$66,$08,$26,$04,$89,$7F,$06,$C8,$03,$27,$02,$CF,$01,$BE; RAW DATA : 	para "더러워지면 안되니까"
+	line $08,$2D,$07,$01,$7F,$05,$18,$01,$6C,$0A,$78,$03,$31,$01,$25,$06,$DF,$0B,$66; RAW DATA : 	line "집에 보관해둘거야!"
 	done
 
-_CeruleanCityText10::
-	text "This is CERULEAN"
-	line "CAVE! Horribly"
-	cont "strong #MON"
-	cont "live in there!"
 
-	para "The #MON LEAGUE"
-	line "champion is the"
-	cont "only person who"
-	cont "is allowed in!"
+_CeruleanCityText10::
+	text $07,$09,$01,$B4,$7F,$05,$4D,$04,$17,$06,$63,$09,$EC,$7F,$03,$1F,$01,$8C,$07,$9C,$06,$DF,$0B,$66; RAW DATA : 	text "여긴 블루시티 동굴이야!"
+	line $06,$F6,$0B,$68,$08,$BB,$02,$3A,$01,$34,$7F,$01,$0D,$0A,$71,$7F,$0A,$27,$09,$2F,$04,$93,$07,$9C; RAW DATA : 	line "엄-청나게 강한 포켓몬이"
+	cont $05,$BC,$01,$4D,$07,$A6,$02,$D9,$01,$4D,$0B,$66; RAW DATA : 	cont "살고있다고!"
+  
+
+	para $07,$09,$01,$B4,$7F,$0A,$27,$09,$2F,$04,$93,$7F,$04,$3E,$01,$A7,$06,$DF,$0B,$66; RAW DATA : 	para "여긴 포켓몬 리그야!"
+	line $07,$20,$08,$27,$7F,$08,$A8,$0A,$67,$06,$F0,$04,$48; RAW DATA : 	line "오직 챔피언만"
+	cont $07,$A4,$07,$B5,$07,$8B,$7F,$0A,$83,$03,$C4,$04,$DE,$07,$8A,$7F,$01,$67,$07,$9C,$08,$26,$0B,$66; RAW DATA : 	cont "입장을 허락받은 곳이지!"
 	done
 
 _CeruleanCityText12::
-	text "CERULEAN CITY"
-	line "A Mysterious,"
-	cont "Blue Aura"
-	cont "Surrounds It"
+	text $07,$9C,$01,$67,$07,$8A,$7F,$05,$4D,$04,$17,$06,$63,$09,$EC; RAW DATA : 	text "이곳은 블루시티"
+	line $05,$4D,$04,$17,$02,$C2,$7F,$04,$D9,$02,$E5,$05,$6B,$7F,$06,$65,$05,$61,$07,$97,$7F,$05,$C6,$01,$C2; RAW DATA : 	line "블루는 바닷빛 신비의 색깔"
 	done
 
 _CeruleanCityText13::
-	text "TRAINER TIPS"
+	text $0B,$6A,$0B,$6A,$7F,$03,$46,$07,$9C,$7F,$03,$27,$02,$C2,$7F,$01,$34,$06,$63,$09,$F7,$0B,$66; RAW DATA : 	text "…… 득이 되는 게시판!"
 
-	para "Pressing B Button"
-	line "during evolution"
-	cont "cancels the whole"
-	cont "process."
+	para $08,$28,$0A,$AD,$08,$0F,$7F,$81,$04,$F6,$09,$E0,$07,$8B,$7F,$02,$A9,$04,$33,$04,$89; RAW DATA : 	para "진화중 B버튼을 누르면"
+	line $08,$28,$0A,$AD,$04,$36,$7F,$08,$0F,$02,$DC,$0A,$72,$7F,$06,$26,$7F,$07,$A6,$06,$60,$02,$CF,$02,$D9,$0B,$66; RAW DATA : 	line "진화를 중단할 수 있습니다!"
 	done
 
+
 _CeruleanCityText16::
-	text "Grass and caves"
-	line "handled easily!"
-	cont "BIKE SHOP"
+	text $07,$AA,$07,$CC,$01,$25,$07,$01,$7F,$02,$EB,$0A,$71,$7F,$01,$2D,$07,$9C,$03,$C3,$04,$89; RAW DATA : 	text "자전거에 대한 것이라면"
+	line $07,$09,$01,$B2,$7F,$04,$CC,$03,$C3,$09,$7C,$7F,$06,$6E,$07,$9C,$09,$7C,$03,$FE,$0B,$66; RAW DATA : 	line "여기 미라클 싸이클로!"
 	done
 
 _CeruleanCityText17::
-	text "CERULEAN CITY"
-	line "#MON GYM"
-	cont "LEADER: MISTY"
-
-	para "The Tomboyish"
-	line "Mermaid!"
-	done
+	text $05,$4D,$04,$17,$06,$63,$09,$EC,$7F,$0A,$27,$09,$2F,$04,$93,$7F,$08,$BC,$07,$80,$01,$6C; RAW DATA : 	text "블루시티 포켓몬 체육관"
+	line $01,$6C,$07,$B5,$7F,$07,$9C,$06,$4D; RAW DATA : 	line "관장 이슬"
+	cont $04,$4B,$01,$6D,$03,$DE,$07,$9C,$7F,$07,$9E,$06,$EE; RAW DATA : 	cont "말괄량이 인어"
+		done
